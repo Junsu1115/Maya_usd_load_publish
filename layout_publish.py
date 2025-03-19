2901b1f9e4d5930312a846b99c9c6ff7e1fd7545
Junsu1115 <djkjs1115@gmail.com>
2025-03-19 22:55:06 +0900

    layout_publish pep8 path edit

--- layout_publish.py
+++ layout_publish.py
@@ -13,9 +13,15 @@ def layout_publish(project_name, seq, shot_num, dept):
         print("해당 dept는 사용할 수 없습니다.")
         return
     
-    shot_root_path = os.path.join(root_directory, project_name,"seq", seq, shot_num)
-    layout_pub_dir = os.path.join(shot_root_path, dept, "pub", "usd")
-    usd_publish_path = os.path.join(layout_pub_dir, f"{shot_num}_{dept}.usda")
+    shot_root_path = os.path.join(
+        root_directory, project_name,"seq", seq, shot_num
+    )
+    layout_pub_dir = os.path.join(
+        shot_root_path, dept, "pub", "usd"
+    )
+    usd_publish_path = os.path.join(
+        layout_pub_dir, f"{shot_num}_{dept}.usda"
+    )
 
     if os.path.exists(layout_pub_dir):
         if not cmds.pluginInfo("mayaUsdPlugin", query=True, loaded=True):
@@ -23,8 +29,12 @@ def layout_publish(project_name, seq, shot_num, dept):
         exporter = LayoutExportUSD(usd_publish_path, seq, shot_num)
         exporter.export()
 
-        pub_path = os.path.join(shot_root_path, dept, "pub", "maya", "scenes")
-        work_path = os.path.join(shot_root_path, dept, "work", "maya", "scenes")
+        pub_path = os.path.join(
+            shot_root_path, dept, "pub", "maya", "scenes"
+        )
+        work_path = os.path.join(
+            shot_root_path, dept, "work", "maya", "scenes"
+        )
 
         version_nums = []
         work_files = os.listdir(work_path)
@@ -35,8 +45,12 @@ def layout_publish(project_name, seq, shot_num, dept):
 
         last_version = max(version_nums, default=0) + 1
 
-        maya_ascii_work_path = os.path.join(work_path, f"{shot_num}_{dept}_v{last_version:03d}.ma")
-        maya_ascii_pub_path = os.path.join(pub_path, f"{shot_num}_{dept}_v{last_version:03d}.ma")
+        maya_ascii_work_path = os.path.join(
+            work_path, f"{shot_num}_{dept}_v{last_version:03d}.ma"
+        )
+        maya_ascii_pub_path = os.path.join(
+            pub_path, f"{shot_num}_{dept}_v{last_version:03d}.ma"
+        )
         cmds.file(rename=maya_ascii_work_path)
         cmds.file(save=True, type="mayaAscii")
         shutil.copy2(maya_ascii_work_path, maya_ascii_pub_path)
